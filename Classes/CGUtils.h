11b8714d7e48f8e9b8328fd25e5a53da2effe64d
Szczepan Kuźniarz <szczepan.kuzniarz@gmail.com>
2014-02-25 00:13:12 +0100

    added CGUtils class

---
+++ Classes/CGUtils.h
@@ -0,0 +1,45 @@
+//
+//  CGUtils.h
+//  SKCommons
+//
+//  Created by skuzniarz on 09/27/13.
+//  Copyright (c) 2013-2014 Szczepan Kuzniarz. All rights reserved.
+//
+
+#import <QuartzCore/QuartzCore.h>
+
+// convert degrees to radians
+#define DEG2RAD(d) (d * M_PI / 180.0)
+
+// convert radians to degrees
+#define RAD2DEG(r) (r * 180.0 / M_PI)
+
+
+@interface CGUtils : NSObject
+
+/**
+ * Round to pixel (1 point on non-Retinach, 0.5 point on Retina).
+ */
++ (CGFloat)roundpx:(CGFloat)value;
+
+/**
+ * Round down to pixel (1 point on non-Retinach, 0.5 point on Retina).
+ */
++ (CGFloat)floorpx:(CGFloat)value;
+
+/**
+ * Round up to pixel (1 point on non-Retinach, 0.5 point on Retina).
+ */
++ (CGFloat)ceilpx:(CGFloat)value;
+
+/**
+ * Convert degrees to radians.
+ */
++ (CGFloat)deg2rad:(CGFloat)degrees;
+
+/**
+ * Convert radians to degrees.
+ */
++ (CGFloat)rad2deg:(CGFloat)radians;
+
+@end
---
+++ Classes/CGUtils.m
@@ -0,0 +1,47 @@
+//
+//  CGUtils.m
+//  SKCommons
+//
+//  Created by skuzniarz on 09/27/13.
+//  Copyright (c) 2013-2014 Szczepan Kuzniarz. All rights reserved.
+//
+
+#import "CGUtils.h"
+#import <UIKit/UIKit.h>
+
+
+@implementation CGUtils
+
++ (CGFloat)roundpx:(CGFloat)value {
+    CGFloat scale = [UIScreen mainScreen].scale;
+    if (scale == 1.f) {
+        return roundf(value);
+    }
+    return (roundf(value * scale) / scale);
+}
+
++ (CGFloat)floorpx:(CGFloat)value {
+    CGFloat scale = [UIScreen mainScreen].scale;
+    if (scale == 1.f) {
+        return floorf(value);
+    }
+    return (floorf(value * scale) / scale);
+}
+
++ (CGFloat)ceilpx:(CGFloat)value {
+    CGFloat scale = [UIScreen mainScreen].scale;
+    if (scale == 1.f) {
+        return ceilf(value);
+    }
+    return (ceilf(value * scale) / scale);
+}
+
++ (CGFloat)deg2rad:(CGFloat)degrees {
+    return (degrees * (CGFloat) M_PI) / 180.f;
+}
+
++ (CGFloat)rad2deg:(CGFloat)radians {
+    return (radians * 180.f) / (CGFloat) M_PI;
+}
+
+@end
